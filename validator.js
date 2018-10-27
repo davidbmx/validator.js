c4426fc461cc9ac25e0da6aa23029419c15cc85b
David <David@MacBook-Pro-de-David.local>
2018-10-27 02:23:06 -0500

    add email validator and change declaration vars

--- validator.js
+++ validator.js
@@ -1,43 +1,50 @@
 (function(){
-	let countErrors = 0,
+	var countErrors = 0,
 		css = 'font-size: 15px;color: red;margin-top: 5px;display: block';
-	const _validators = {},
-		arrayReturn = [],
+	var _validators = {},
 		defaultMessage = {
 			required: 'This field is required.',
 			number: 'This field accept only numbers.',
 			letters: 'This field accept only letters.',
-			_default: 'Exists one error in this field.'
+			email: 'Email format is wrong.',
+			_default: 'This field have one error.'
 		};
 	
 	_validators.required = function(object) {
 		validateTemplate(object, 'required', function (value) {
 			return value ? true : false;
 		});
-	}
+	};
 	
 	_validators.number = function(object) {
 		validateTemplate(object, 'number', function (value) {
 			return !isNaN(value);
 		});
-	}
+	};
 	
 	_validators.letters = function(object) {
-		const letters = /^[A-Za-z]+$/;
+		var patter = /^[A-Za-z]+$/;
 		validateTemplate(object, 'letters', function (value) {
-			return value.match(letters);
+			return value.match(patter);
 		});
-	}
+	};
+
+	_validators.email = function(object) {
+		var patter = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+		validateTemplate(object, 'email', function (value) {
+			return value.match(patter);
+		});
+	};
 	
 	function execCustomValidate(object) {
 		validateTemplate(object, '_default', object.fn);
 	}
 	
 	function validateTemplate(object, messageDefault, fn) {
-		for(let i = 0, x = object.fields.length; i < x; i += 1) {
-			let el = getEl(object.fields[i]);
+		for(var i = 0, x = object.fields.length; i < x; i += 1) {
+			var el = getEl(object.fields[i]);
 			if (!fn(el.value)) {
-				let message = typeof object.messages !== 'undefined' && object.messages[i] ? object.messages[i] : defaultMessage[messageDefault];
+				var message = typeof object.messages !== 'undefined' && object.messages[i] ? object.messages[i] : defaultMessage[messageDefault];
 				displayErrors(el, message);
 				countErrors += 1;
 			}
@@ -45,12 +52,12 @@
 	}
 	
 	function displayErrors(el, message) {
-		const errorEl = createEl('span', message);
+		var errorEl = createEl('span', message);
 		insertBefore(el, errorEl);
 	}
 	
 	function createEl(el, message) {
-		const _el = document.createElement(el);
+		var _el = document.createElement(el);
 		_el.className = '_validate_forms_';
 		_el.textContent = message;
 		_el.style.cssText = css;
@@ -62,15 +69,15 @@
 	}
 	
 	function insertBefore(el, before) {
-		const parent = el.parentNode;
+		var parent = el.parentNode;
 		parent.insertBefore(before, el.nextSibling);
 	}
 	
 	function clearErrors() {
-		let errors = document.querySelectorAll('._validate_forms_');
+		var errors = document.querySelectorAll('._validate_forms_');
 		errors = Object.values(errors);
 		errors.forEach(function(el) {
-			let parent = el.parentNode;
+			var parent = el.parentNode;
 			parent.removeChild(el);
 		});
 	}
@@ -81,7 +88,7 @@
 		}
 		countErrors = 0;
 		clearErrors();
-		for(let i in object) {
+		for(var i in object) {
 			if (object.hasOwnProperty(i) && _validators.hasOwnProperty(i)) {
 				_validators[i](object[i]);
 			} else if (typeof object[i] === 'object' && typeof object[i].fn === 'function') {
@@ -94,12 +101,12 @@
 	
 	window.Validator = function (options) {
 		if (options) {
-			for(let i in options) {
+			for(var i in options) {
 				if (options.hasOwnProperty(i) && defaultMessage.hasOwnProperty(i)) {
 					defaultMessage[i] = options[i];
 				}
 			}
 		}
 		return validator;
-	}
+	};
 })();
